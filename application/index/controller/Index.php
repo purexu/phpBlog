1280dc894715c28422dbf43fed3dc7910b4dcdd8
purexu <2287659301@qq.com>
2019-12-10 11:47:54 +0800

    test hooks~

--- application/index/controller/Index.php
+++ application/index/controller/Index.php
@@ -2,6 +2,11 @@
 namespace app\index\controller;
 use app\Common\Controller\BlogBaseController;
 class Index extends BlogBaseController{
+
+    public function __construct() {
+        parent::__construct();
+    }
+
     public function index(){
         return "hello thinkphp";
     }
